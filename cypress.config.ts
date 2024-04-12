786b0c68c91b852f85363bd5393909585ae417cb
Macq <q.macq@motoblouz.com>
2024-04-12 12:41:48 +0200

    feat: add renovate bot and update dependencies

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3006',
     specPattern: 'cypress/e2e/**/*.{js,ts}'
   }
 });
